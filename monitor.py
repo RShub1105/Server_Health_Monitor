da9f20c38b461be34ea1774d8ca8cc5eb7cbd4d0
Rahul sharma <113161501+RShub1105@users.noreply.github.com>
2025-10-01 14:25:24 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,12 +1,16 @@
 import psutil
 from datetime import datetime
 
+
 def get_system_stats():
     cpu = psutil.cpu_percent(interval=1)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage("/").percent
     return cpu, memory, disk
 
+
 def log_stats(cpu, memory, disk, logfile="logs/system_log.txt"):
     with open(logfile, "a") as f:
-        f.write(f"{datetime.now()} | CPU={cpu}% | Memory={memory}% | Disk={disk}%\n")
+        f.write(
+            f"{datetime.now()} | CPU={cpu}% | Memory={memory}% | Disk={disk}%\n"
+        )
